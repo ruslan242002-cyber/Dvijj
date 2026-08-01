1dd463605229e8553820ac6ca5fd3144cfab3cb6
ruslan242002-cyber <ruslan242002@mail.ru>
2026-08-01 09:34:33 +0300

    Create seeded-rng.js

---
+++ engine/seeded-rng.js
@@ -0,0 +1,10 @@
+'use strict';
+function makeRng(seed) {
+  let s = seed % 2147483647;
+  if (s <= 0) s += 2147483646;
+  return function () {
+    s = (s * 16807) % 2147483647;
+    return (s - 1) / 2147483646;
+  };
+}
+module.exports = { makeRng };
