d8d3894a419fd790270adb59be490469a5aa0977
ruslan242002-cyber <ruslan242002@mail.ru>
2026-08-07 11:30:15 +0300

    Update location-images.js

--- game/location-images.js
+++ game/location-images.js
@@ -3,7 +3,7 @@ const LOCATION_IMAGES = {
   'Приют': { bridge: 'priyut-shtab.jpg', repair: null, decon: 'decon-generic.jpg', cantina: 'priyut-bar.jpg', gates: 'priyut-gates.jpg', station: 'priyut-overview.jpg', market: 'priyut-market.jpg' },
   'Терминус': { bridge: 'terminus-most.jpg', repair: null, decon: 'decon-generic.jpg', cantina: null, gates: 'terminus-gates.jpg', station: 'terminus-hub.jpg', market: null },
   'Арсенал': { bridge: 'arsenal-shtab.jpg', repair: 'arsenal-repair.jpg', decon: 'decon-generic.jpg', cantina: 'arsenal-bar.jpg', gates: 'arsenal-gates.jpg', station: 'arsenal-hub.jpg', market: null },
-  'Вуаль': { bridge: 'vual-shtab.jpg', repair: 'vual-scavenger.jpg', decon: 'decon-generic.jpg', cantina: null, gates: 'vual-gates.jpg', station: 'vual-hub.jpg', market: null },
+  'Вуаль': { bridge: 'vual-shtab.jpg', repair: 'vual-scavenger.jpg', decon: 'decon-generic.jpg', cantina: 'vual-bar.jpg', gates: 'vual-gates.jpg', station: 'vual-hub.jpg', market: null, housing: 'vual-housing.jpg' },
 };
 function imageForLocation(key, faction) {
   const stationImages = LOCATION_IMAGES[faction];
