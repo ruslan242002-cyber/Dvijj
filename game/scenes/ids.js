b79581e5e264943c6b338e71ac90ecef28a290de
Ruslan <ruslan242002@mail.ru>
2026-09-19 16:57:45 +0300

    Create ids.js

---
+++ game/scenes/ids.js
@@ -0,0 +1,118 @@
+'use strict';
+
+/**
+ * Константы имён сцен. Раньше сцена была голой строкой в каждом
+ * nextState — опечатка вроде 'statoin' компилировалась молча и тихо
+ * ломала переход только в рантайме. Теперь опечатка — это ReferenceError
+ * на этапе require(), а не необъяснимое "бот завис" у игрока.
+ *
+ * Использование не обязательно везде и сразу — старые строки продолжат
+ * работать (это те же самые значения), но новый код лучше писать через
+ * SCENES.XXX.
+ */
+const SCENES = Object.freeze({
+  START: 'start',
+  ASK_NAME: 'ask_name',
+  ASK_FACTION: 'ask_faction',
+  QUEST_REPORT: 'quest_report',
+  QUEST_SHOP: 'quest_shop',
+  QUEST_GATES: 'quest_gates',
+
+  STATION: 'station',
+  DISTRICT_HUB: 'district_hub',
+
+  LOC_BRIDGE: 'loc_bridge',
+  LORE_MYTHOS: 'lore_mythos',
+  PASSIVE_MANAGEMENT: 'passive_management',
+  FACTION_TRANSFER: 'faction_transfer',
+  SHIP_RETURNING: 'ship_returning',
+  PACK_PRE_COMBAT: 'pack_pre_combat',
+  PACK_COMBAT: 'pack_combat',
+  PACK_TARGET: 'pack_target',
+  DISTRESS_CHOICE: 'distress_choice',
+  RESONANCE_PEDESTAL_CHOICE: 'resonance_pedestal_choice',
+  TERMINAL_HACK_CHOICE: 'terminal_hack_choice',
+  ECHO_PLAYBACK_CHOICE: 'echo_playback_choice',
+  REACTION_HAZARD_CHOICE: 'reaction_hazard_choice',
+  CORRUPTED_AI_CHOICE: 'corrupted_ai_choice',
+  GUILD_HUB: 'guild_hub',
+  GUILD_MEMBERS: 'guild_members',
+  GUILD_CREATE_NAME: 'guild_create_name',
+  GUILD_JOIN_NAME: 'guild_join_name',
+  GUILD_DONATE_CREDITS: 'guild_donate_credits',
+  GUILD_DONATE_RESOURCE: 'guild_donate_resource',
+  GUILD_WITHDRAW_RESOURCE: 'guild_withdraw_resource',
+  GUILD_UPGRADE: 'guild_upgrade',
+  GUILD_PROJECTS_LIST: 'guild_projects_list',
+  GUILD_PROJECT_DETAIL: 'guild_project_detail',
+  GUILD_PROJECT_DONATE_CREDITS: 'guild_project_donate_credits',
+  GUILD_PROJECT_DONATE_RESOURCE: 'guild_project_donate_resource',
+  DICE_GAME: 'dice_game',
+  TRADE_ROUTES: 'trade_routes',
+  MENTOR_CLASS: 'mentor_class',
+  REFORGE_PICK: 'reforge_pick',
+  REFORGE_TARGET: 'reforge_target',
+  BOSS_HUB: 'boss_hub',
+  BOSS_COMBAT: 'boss_combat',
+  RAID_LOBBY: 'raid_lobby',
+  RAID_BATTLE: 'raid_battle',
+  NPC_PEOPLE: 'npc_people',
+  NPC_QUEST: 'npc_quest',
+  NAMED_CHARACTER: 'named_character',
+  MINIGAME_SHIP_DIAGNOSTICS: 'minigame_ship_diagnostics',
+  MINIGAME_POWER_CONTROL: 'minigame_power_control',
+  MINIGAME_SENSOR_LAYERS: 'minigame_sensor_layers',
+  MINIGAME_ARCHIVE: 'minigame_archive',
+  MINIGAME_SECONDARY_STATUS: 'minigame_secondary_status',
+  QUEST_SHYOPOT: 'quest_shyopot',
+  LOC_REPAIR: 'loc_repair',
+  LOC_DECON: 'loc_decon',
+  LOC_CANTINA: 'loc_cantina',
+  CONTRACTS: 'contracts',
+  LOC_GATES: 'loc_gates',
+  LOC_GATES_TRAVEL: 'loc_gates_travel',
+  WORKSHOP: 'workshop',
+  SHIPYARD: 'shipyard',
+  STEALTH_EXPLORE: 'stealth_explore',
+  CURATOR_QUEST: 'curator_quest',
+
+  JOURNEY: 'journey',
+  JOURNEY_CONTINUE: 'journey_continue',
+  EXPLORATION_EVENT_CHOICE: 'exploration_event_choice',
+  ANOMALY_CHOICE: 'anomaly_choice',
+  ANOMALY_PUZZLE: 'anomaly_puzzle',
+  NEUTRAL_ENCOUNTER: 'neutral_encounter',
+
+  PRE_COMBAT: 'pre_combat',
+  COMBAT: 'combat',
+  COMBAT_STIM_SELECT: 'combat_stim_select',
+
+  MARKET_HUB: 'market_hub',
+  MARKET_SELL_PICK: 'market_sell_pick',
+  MARKET_SELL_PRICE: 'market_sell_price',
+  MARKET_BUY_ORDERS_HUB: 'market_buy_orders_hub',
+  MARKET_BUY_ORDER_ITEM: 'market_buy_order_item',
+  MARKET_BUY_ORDER_QTY: 'market_buy_order_qty',
+  MARKET_BUY_ORDER_PRICE: 'market_buy_order_price',
+  MARKET_MY_LISTINGS: 'market_my_listings',
+  MARKET_ITEM_BOOK: 'market_item_book',
+  MARKET_BUY_QTY: 'market_buy_qty',
+  PVP_MENU: 'pvp_menu',
+  PVP_DUEL: 'pvp_duel',
+  HOUSING_HUB: 'housing_hub',
+  HOUSING_ITEM_PICK: 'housing_item_pick',
+
+  SHIP_TRAVEL: 'ship_travel',
+  SHIP_PRE_COMBAT: 'ship_pre_combat',
+  SHIP_COMBAT: 'ship_combat',
+  SHIP_TRADER: 'ship_trader',
+
+  VEIN_HUB: 'vein_hub',
+  VEIN_ATTACK_LIST: 'vein_attack_list',
+  VEIN_PVP_COMBAT: 'vein_pvp_combat',
+  VEIN_MONSTER_COMBAT: 'vein_monster_combat',
+  VEIN_BOSS_WAIT: 'vein_boss_wait',
+  VEIN_BOSS_COMBAT: 'vein_boss_combat',
+});
+
+module.exports = { SCENES };
